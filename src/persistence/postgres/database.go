c0a5e9bd7ae136af853d648eea38f51b51e0bf40
Sixto Ortega <ortegasixto7@gmail.com>
2023-08-17 18:14:08 -0400

    Adding go env

--- src/persistence/postgres/database.go
+++ src/persistence/postgres/database.go
@@ -1,6 +1,8 @@
 package postgres
 
 import (
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -8,9 +10,8 @@ import (
 var Database *gorm.DB
 
 func Init() {
-
-	url := "host=localhost user=postgres password=12345678 dbname=go-bank port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	databaseUrl := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(postgres.Open(databaseUrl), &gorm.Config{})
 	if err != nil {
 		panic("Error In Database Connection")
 	}
--- src/server.go
+++ src/server.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/goccy/go-json"
+	"github.com/joho/godotenv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/ortegasixto7/go-bank-api/src/controllers"
@@ -9,6 +13,16 @@ import (
 )
 
 func main() {
+	var PORT string = ":8005"
+	var GO_ENV string = os.Getenv("GO_ENV")
+	if GO_ENV == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+		PORT = os.Getenv("PORT")
+	}
+
 	postgres.Init()
 	var userController controllers.UserController
 	app := fiber.New(fiber.Config{
@@ -24,6 +38,6 @@ func main() {
 
 	app.Post("/users/sign-up/v1", userController.SignUp)
 
-	app.Listen(":3000")
+	app.Listen(PORT)
 
 }
